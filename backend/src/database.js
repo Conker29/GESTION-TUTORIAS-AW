4fb86787319948d3be0a09f53c1a8fc9cecef561
Conker29 <pablo-e1@hotmail.com>
2025-06-17 12:27:18 -0500

    Update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -3,20 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config(); 
 
-mongoose.set('strictQuery', true) //que sea extricta
+mongoose.set('strictQuery', true);
 
-//conexion con la base
-const connection = async()=>{ 
-try{
-    await mongoose.connect(process.env.MONGODB_URL_PRODUCTION,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+const connection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL_PRODUCTION);
     console.log('✅ Conectado a MongoDB Atlas');
-}catch (error) {
+  } catch (error) {
     console.error('❌ Error de conexión a MongoDB:', error.message);
     process.exit(1);
-    }
-}
+  }
+};
 
-export default  connection;
+export default connection;

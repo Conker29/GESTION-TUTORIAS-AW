f18a44670e82239c206212fc2fde0473a6facc43
dannamishelle23 <dannamishelle.23@gmail.com>
2025-06-29 14:00:41 -0500

    JWT.js fixed

--- backend/src/middlewares/JWT.js
+++ backend/src/middlewares/JWT.js
@@ -15,7 +15,7 @@ const verificarTokenJWT = async (req, res, next) => {
     try {
         const token = authorization.split(" ")[1];
         const { id, rol } = jwt.verify(token,process.env.JWT_SECRET)
-        if (rol === "estudiante") {
+        if (rol === "Estudiante") {
             req.EstudianteBDD = await Estudiante.findById(id).lean().select("-password")
             next()
         }

4492c2308996bc540f298b1e317404ff29a7a55a
dannamishelle23 <dannamishelle.23@gmail.com>
2025-06-29 13:58:25 -0500

    agregado rutas de actualizar perfil y password

--- backend/src/routers/estudiante_routes.js
+++ backend/src/routers/estudiante_routes.js
@@ -1,11 +1,14 @@
 import {Router} from 'express'
-import { confirmarMail, recuperarPassword, registro, comprobarTokenPassword, crearNuevoPassword, login } from '../controllers/estudiante_controller.js'
+import { confirmarMail, recuperarPassword, registro, comprobarTokenPassword, crearNuevoPassword, login, perfil, actualizarPerfil,
+    actualizarPassword} from '../controllers/estudiante_controller.js'
+import { verificarTokenJWT } from '../middlewares/JWT.js'
+
 const router = Router()
 
+//Rutas públicas
 router.post('/registro',registro)
 
 router.get('/confirmar/:token', confirmarMail)
-
 
 router.post('/recuperarpassword',recuperarPassword)
 
@@ -14,5 +17,12 @@ router.get('/recuperarpassword/:token',comprobarTokenPassword)
 router.post('/nuevopassword/:token',crearNuevoPassword)
 
 router.post ('/login',login)
+
+//Rutas privadas
+router.get('/perfil',verificarTokenJWT,perfil)
+
+router.put('/estudiante/:id',verificarTokenJWT,actualizarPerfil)
+
+router.put('/estudiante/actualizarpassword/:id',verificarTokenJWT,actualizarPassword)
 
 export default router
